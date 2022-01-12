a56151c410e4864ec38145464a947219ea1a88ef
Adrian Lactuan <aplactuan@gmail.com>
2022-01-12 16:36:28 +0800

    Add inertia link instead of usual href

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,10 +5,15 @@
             {{ framework }}
         </li>
     </ul>
+    <Nav></Nav>
 </template>
 
 <script>
+import Nav from '../Shared/Nav.vue';
 export default {
+    components: {
+        Nav
+    },
     props: {
         name: String,
         frameworks: Array
---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>This is the users page</h2>
+    <Nav></Nav>
+</template>
+
+<script>
+import Nav from "../Shared/Nav.vue";
+export default {
+    components: {
+        Nav
+    }
+}
+</script>
